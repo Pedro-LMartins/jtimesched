e9b2ff7ca2a461e8de6d51618e17366b9e3cc7a4
Jmigfer7 <jmigfer7@gmail.com>
2021-03-08 11:59:38 +0000

    Typos in annotations and some strings

--- src/test/java/jtimesched/ProjectTableModelTest.java
+++ src/test/java/jtimesched/ProjectTableModelTest.java
@@ -31,20 +31,20 @@ class ProjectTableModelTest {
 	class TestGetColumnName {
 
 		@Test
-		@DisplayName("Test getColumnName with negative column index")
+		@DisplayName("getColumnName with negative column index")
 		public void testColumnNegative() {
 			assertEquals("", table.getColumnName(-1), "If column cannot be found, should return an empty string");
 		}
 
 		@Test
-		@DisplayName("Test getColumnName with an existent column index")
+		@DisplayName("getColumnName with an existent column index")
 		public void testColumnNotNull() {
 			assertNotNull(table.getColumnName(ProjectTableModel.COLUMN_TITLE),
 					"Get of an existent column should return a String");
 		}
 
 		@Test
-		@DisplayName("Test getColumnName with an inexistent positive column index")
+		@DisplayName("getColumnName with an inexistent positive column index")
 		public void testColumnInexistent() {
 			assertEquals("", table.getColumnName(table.getColumnCount() + 100),
 					"If column cannot be found, should return an empty string");
@@ -57,16 +57,16 @@ class ProjectTableModelTest {
 	class TestGetProjectAt {
 
 		@Test
-		@DisplayName("Test getProjectAt with negative row index")
+		@DisplayName("getProjectAt with negative row index")
 		public void testGetProjectAtNegativeLine() {
 			assertThrows(ProjectException.class, () -> table.getProjectAt(-1),
 					"Obtaining an inexistent project should raise an exception");
 		}
 
 		@Test
-		@DisplayName("Test getProjectAt with existent project")
+		@DisplayName("getProjectAt with existent project")
 		public void testGetProject() {
-			Project expected = new Project("expected project title");
+			Project expected = new Project("title");
 			table.addProject(expected);
 
 			Project actual = table.getProjectAt(0);
@@ -75,7 +75,7 @@ class ProjectTableModelTest {
 		}
 
 		@Test
-		@DisplayName("Test getProjectAt with inexisting project")
+		@DisplayName("getProjectAt with inexisting project")
 		public void testGetProjectAtInexistingLine() {
 			assertThrows(ProjectException.class, () -> table.getProjectAt(100),
 					"Obtaining an inexistent project should raise an exception");
@@ -88,16 +88,16 @@ class ProjectTableModelTest {
 	class TestGetValueAt {
 
 		@Test
-		@DisplayName("Test getValueAt with a negative row index")
+		@DisplayName("getValueAt with a negative row index")
 		public void testGetValueAtWithNegativeLine() {
 			assertThrows(ProjectException.class, () -> table.getValueAt(-1, ProjectTableModel.COLUMN_TITLE),
 					"Obtaining an inexistent project should raise an exception");
 		}
 
 		@Test
-		@DisplayName("Test getValueAt with valid input (row and column)")
+		@DisplayName("getValueAt with valid input (row and column)")
 		public void testGetValueAtWithExistingProject() {
-			Project project = new Project("project title");
+			Project project = new Project("title");
 			String expected = project.getTitle();
 			table.addProject(project);
 
@@ -105,25 +105,25 @@ class ProjectTableModelTest {
 		}
 
 		@Test
-		@DisplayName("Test getValueAt with row index unexisting project")
+		@DisplayName("getValueAt with row index inexisting project")
 		public void testGetValueAtWithInexistingProject() {
 			assertThrows(ProjectException.class, () -> table.getValueAt(0, ProjectTableModel.COLUMN_TITLE),
 					"Obtaining an inexistent project should raise an exception");
 		}
 
 		@Test
-		@DisplayName("Test getValueAt with negative column index")
+		@DisplayName("getValueAt with negative column index")
 		public void testGetValueAtWithNegativeColumn() {
-			table.addProject(new Project("project title"));
+			table.addProject(new Project("title"));
 
 			assertThrows(ProjectException.class, () -> table.getValueAt(0, -1),
 					"Obtaining an inexistent column should raise an exception");
 		}
 
 		@Test
-		@DisplayName("Test getValueAt with an invalid column index")
+		@DisplayName("getValueAt with an invalid column index")
 		public void testGetValueAtWithNonExistentColumn() {
-			table.addProject(new Project("project title"));
+			table.addProject(new Project("title"));
 
 			assertThrows(ProjectException.class, () -> table.getValueAt(0, table.getColumnCount() + 100),
 					"If column cannot be found, should should raise an exception");
@@ -132,44 +132,44 @@ class ProjectTableModelTest {
 	}
 
 	@Nested
-	@DisplayName("Test for isCellEditable method")
+	@DisplayName("Tests for isCellEditable method")
 	class TestIsCellEditable {
 
 		@Test
-		@DisplayName("Test isCellEditable with a negative row index")
+		@DisplayName("isCellEditable with a negative row index")
 		public void testIsCellEditableWithNegativeLine() {
 			assertThrows(ProjectException.class, () -> table.isCellEditable(-1, ProjectTableModel.COLUMN_TITLE),
 					"Obtaining an inexistent project should raise an exception");
 		}
 
 		@Test
-		@DisplayName("Test isCellEditable with valid input (row and column)")
+		@DisplayName("isCellEditable with valid input (row and column)")
 		public void testIsCellEditableWithExistingProject() {
-			table.addProject(new Project("project title"));
+			table.addProject(new Project("title"));
 
 			assertTrue(table.isCellEditable(0, ProjectTableModel.COLUMN_TITLE));
 		}
 
 		@Test
-		@DisplayName("Test isCellEditable with row index of unexisting project")
+		@DisplayName("isCellEditable with row index of inexisting project")
 		public void testIsCellEditableWithInexistingProject() {
 			assertThrows(ProjectException.class, () -> table.getValueAt(0, ProjectTableModel.COLUMN_TITLE),
 					"Obtaining an inexistent project should raise an exception");
 		}
 
 		@Test
-		@DisplayName("Test isCellEditable with negative column index")
+		@DisplayName("isCellEditable with negative column index")
 		public void testIsCellEditableNegativeColumn() {
-			table.addProject(new Project("project title"));
+			table.addProject(new Project("title"));
 
 			assertThrows(ProjectException.class, () -> table.isCellEditable(0, -1),
 					"Obtaining an inexistent column should raise an exception");
 		}
 
 		@Test
-		@DisplayName("Test isCellEditable with an invalid column index")
+		@DisplayName("isCellEditable with an invalid column index")
 		public void testIsCellEditableWithNonExistentColumn() {
-			table.addProject(new Project("project title"));
+			table.addProject(new Project("title"));
 
 			assertThrows(ProjectException.class, () -> table.isCellEditable(0, table.getColumnCount() + 100),
 					"If column cannot be found, should should raise an exception");
