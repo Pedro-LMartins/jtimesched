9a9cad7084a96bc48101245e780cb257b44b6865
Jmigfer7 <jmigfer7@gmail.com>
2021-04-25 19:09:46 +0100

    Tests for coverage

--- src/test/java/jtimesched/ProjectTest.java
+++ src/test/java/jtimesched/ProjectTest.java
@@ -1,5 +1,6 @@
 package jtimesched;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import de.dominik_geyer.jtimesched.project.Project;
 import de.dominik_geyer.jtimesched.project.ProjectException;
+
 
 @DisplayName("Tests for Project class")
 class ProjectTest {
@@ -41,6 +43,141 @@ class ProjectTest {
 		void testToggleWithPausedProject() {
 				project.toggle();
 				assertTrue(project.isRunning());
+		}
+	}
+	
+	@Nested
+	@DisplayName("Tests for adjustSecondsToday")
+	class TestAdjustSecondsToday {
+		
+		@Test
+		@DisplayName("getColumnName with an existent column index")
+		public void testColumnNotNull() {
+			project.adjustSecondsToday(10);
+			assertEquals(10, project.getSecondsToday(),
+					"Get of an existent column should return a String");
+		}		
+	}
+	
+	@Nested
+	@DisplayName("Tests for adjustSecondsToday")
+	class TestResetToday {
+		
+		@Test
+		@DisplayName("getColumnName with an existent column index")
+		public void testColumnNotNull() {
+			project.resetToday();
+			assertEquals(0, project.getSecondsToday()," Get of an existent column should return a String");
+			assertEquals(0, project.getQuotaToday(),
+					"Get of an existent column should return a String");
+		}		
+	}
+	
+	@Nested
+	@DisplayName("Tests for exception branches on functions")
+	class Testexceptions {
+		
+		@Test
+		@DisplayName("Test start with a running project")
+		void teststartWithRunningProject() {
+			try {
+				project.start();
+				assertThrows(ProjectException.class, () -> project.start(),
+						"Should throw an exception when trying to start when the program is already started");
+			}
+			catch (ProjectException e) {
+				fail(e);
+			}
+			
+		}
+		
+		
+//		@Test
+//		@DisplayName("Test getElapsedSeconds with a running project")
+//		void testgetElapsedSeconds() {
+//			try {
+//				project.getElapsedSeconds();
+//				assertThrows(ProjectException.class, () -> project.getElapsedSeconds(),
+//						"Should throw an exception when trying to get ElapsedSeconds when the program is running");
+//			}
+//			catch (ProjectException e) {
+//				fail(e);
+//			}
+//			
+//			
+//		}
+		
+		@Test
+		@DisplayName("Test pause with a paused project")
+		void testpauseWithpausedProject() {
+			try {
+				project.pause();
+				assertThrows(ProjectException.class, () -> project.pause(),
+						"Should throw an exception when trying to pause when the program is already paused");
+			}
+			catch (ProjectException e) {
+				fail(e);
+			}
+			
+		}
+		
+		@Test
+		@DisplayName("Test getSecondsToday with a started project")
+		void testgetSecondsTodayWithstartedProject() {
+			try {
+				project.start();
+				assertEquals(0, project.getSecondsToday(),
+						"Should return 0 since zero seconds are being counted but it is started");
+			}
+			catch (ProjectException e) {
+				fail(e);
+			}
+			
+		}
+		
+		@Test
+		@DisplayName("Test getSecondsOverall with a started project")
+		void testgetSecondsOverallWithstartedProject() {
+			try {
+				project.start();
+				assertEquals(0, project.getSecondsOverall(),
+						"Should return 0 since zero seconds are being counted but it is started");
+			}
+			catch (ProjectException e) {
+				fail(e);
+			}
+			
+		}
+	}
+	
+	
+	@Nested
+	@DisplayName("Tests for setting seconds functions with negative numbers")
+	class Testseconds {
+		
+		@Test
+		@DisplayName("Test setSecondsOverall with a negative number")
+		void testsetSecondsOverallWithNegativeNumber() {
+			project.setSecondsOverall(-1);
+			assertEquals(0, project.getSecondsOverall()," Seconds should be set to 0");
+			
+		}
+		
+		
+		@Test
+		@DisplayName("Test setsetSecondsToday with a negative number")
+		void testsetSecondsTodayWithNegativeNumber() {
+			project.setSecondsToday(-1);
+			assertEquals(0, project.getSecondsToday()," Seconds should be set to 0");
+			
+		}
+		
+		@Test
+		@DisplayName("Test adjustSecondsToday with a negative number")
+		void testadjustSecondsTodayWithNegativeNumber() {
+			project.adjustSecondsToday(-1);
+			assertEquals(0, project.getSecondsToday()," Seconds should be set to 0");
+			
 		}
 	}
 
--- src/test/java/jtimesched/ProjectTimeTest.java
+++ src/test/java/jtimesched/ProjectTimeTest.java
@@ -1,11 +1,15 @@
 package jtimesched;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.text.ParseException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import de.dominik_geyer.jtimesched.project.ProjectException;
 import de.dominik_geyer.jtimesched.project.ProjectTime;
 
 @DisplayName("Tests for ProjectTime class")
@@ -57,4 +61,46 @@ class ProjectTimeTest {
 		}
 
 	}
+	
+	@Nested
+	@DisplayName("Tests for TestparseSeconds method")
+	class TestparseSeconds {
+
+		@Test
+		@DisplayName("parseSeconds with value 59 seconds")
+		public void TestParseSecondsNegative() throws ParseException { 
+			assertEquals(59, ProjectTime.parseSeconds("00:00:59"),
+					"Should return 59");
+		}
+		@Test
+		@DisplayName("parseSeconds with value invalid")
+		public void TestParseSecondsexception() throws ParseException { 
+			assertThrows(ProjectException.class, () -> ProjectTime.parseSeconds("99999999"),
+					"Should return an exception");
+		}
+	}
+	
+	@Nested
+	@DisplayName("Tests for formatDate method")
+	class formatDate {
+		
+		@Test
+		@DisplayName("FormatDate with null value")
+		public void TestFormatDate() {
+			assertThrows(ProjectException.class, () -> ProjectTime.formatDate(null),
+					"Should return an exception");   
+	}
+	
+	@Nested
+	@DisplayName("Tests for TestparseDate method")
+	class TestparseDate {
+
+		@Test
+		@DisplayName("parsedate with value -1")
+		public void TestParseDate() throws ParseException {
+			assertThrows(ProjectException.class, () -> ProjectTime.parseDate("99999999"),
+					"weird format should return an exception");
+		}
+	}
+}
 }
